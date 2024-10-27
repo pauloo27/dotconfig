98c2ece26d0d0de4936450c3849db1d052032d74
Paulo <git@pauloo27.dev>
2024-10-27 02:46:31 -0300

    feat: add debug msg

--- .config/waybar/launch.sh
+++ .config/waybar/launch.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 WM=$XDG_SESSION_DESKTOP
 
+echo "Lauching waybar for $WM"
+
 jq -s '.[0] * .[1]' ~/.config/waybar/config.base.jsonc ~/.config/waybar/config.$WM.jsonc > ~/.config/waybar/config.jsonc
 waybar -c ~/.config/waybar/config.jsonc -s ~/.config/waybar/style.$WM.css
